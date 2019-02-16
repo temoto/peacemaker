4c8ddd5637450ede3b8748c216a3a2489dbc2175
Sergey Shepelev <temotor@gmail.com>
2019-02-16 13:18:59 +0500

    command-line argument parsing was broken
    https://github.com/temoto/peacemaker/issues/2

--- main.go
+++ main.go
@@ -28,12 +28,20 @@ type ProcessLister interface {
 	List() ([]*Process, error)
 }
 
-var (
-	flagDebug    = flag.Bool("-debug", false, "")
-	pollInterval = flag.Duration("-interval", 1666*time.Millisecond, "")
-	limitMiB     = flag.Float64("-limit-mb", 100, "")
-	limitPercent = flag.Float64("-limit-percent", 5, "")
-)
+func main() {
+	flagDebug := flag.Bool("debug", false, "")
+	pollInterval := flag.Duration("interval", 1666*time.Millisecond, "")
+	limitMiB := flag.Float64("limit-mb", 100, "")
+	limitPercent := flag.Float64("limit-percent", 5, "")
+	flag.Parse()
+	log.Printf("Configuration: pollInterval=%s limitMiB=%.1f limitPercent=%.2f%%\n",
+		*pollInterval, *limitMiB, *limitPercent)
+
+	for {
+		step(*flagDebug, *limitMiB, *limitPercent)
+		time.Sleep(*pollInterval)
+	}
+}
 
 func chooseVictimByMemory(ps []*Process) *Process {
 	if len(ps) == 0 {
@@ -50,7 +58,7 @@ func chooseVictimByMemory(ps []*Process) *Process {
 	return result
 }
 
-func step() {
+func step(debug bool, limitMiB, limitPercent float64) {
 	ps, err := ProcSource("/proc").List()
 	if err != nil {
 		log.Fatalln(err)
@@ -64,12 +72,12 @@ func step() {
 	availableMiB := float64(mi["MemAvailable"]) / MiB
 	totalMiB := float64(mi["MemTotal"]) / MiB
 	availablePercent := availableMiB / totalMiB * 100
-	if *flagDebug {
+	if debug {
 		log.Printf("Available memory: %.1f / %.1f MiB = %.1f%%\n",
 			availableMiB, totalMiB, availablePercent)
 	}
 
-	if availableMiB < *limitMiB || availablePercent < *limitPercent {
+	if availableMiB < limitMiB || availablePercent < limitPercent {
 		log.Println("Memory limit")
 		victim := chooseVictimByMemory(ps)
 		if victim == nil {
@@ -78,16 +86,5 @@ func step() {
 		log.Printf("  going to kill %s pid=%d memory=%d (%.1f%%)\n",
 			victim.Name, victim.Pid, victim.MemReal/MiB, float32(victim.MemReal)*100.0/float32(mi["MemTotal"]))
 		victim.Terminate()
-	}
-}
-
-func main() {
-	flag.Parse()
-	log.Printf("Configuration: pollInterval=%s limitMiB=%.1f limitPercent=%.2f%%\n",
-		*pollInterval, *limitMiB, *limitPercent)
-
-	for {
-		step()
-		time.Sleep(*pollInterval)
 	}
 }
